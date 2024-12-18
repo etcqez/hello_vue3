d1aafb07d7887e4ddc1bfaf406cf959198ed1023
etcqez <etcqez@outlook.com>
2024-12-18 14:43:11 +0800

    更新配置文件

---
+++ src/components/Person.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="person">
+        <h2>姓名：{{ name }}</h2>
+        <h2>年齡：{{ age }}</h2>
+        <button @click="changeName">修改名字</button>
+        <button @click="changeAge">修改年龄</button>
+        <button @click="showTel">查看联系方式</button>
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'Person',
+    data() {
+        return {
+            name: '张三',
+            age: 18,
+            tel: '100000'
+        }
+    },
+    methods: {
+        changeName() {
+            this.name = 'zhang-san'
+        },
+        changeAge() {
+            this.age += 1
+        },
+        showTel() {
+            alert(this.tel)
+        },
+
+    }
+
+}
+</script>
+
+<style>
+.person {
+    background: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+
+}
+
+button {
+    margin: 0 20px;
+}
+</style>
---
+++ 备份/01_自已写一个App组件/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h1>你不好啊！ </h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.app {
+  background: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
